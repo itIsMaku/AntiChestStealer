9c6ddc0fc8eb574c9ebc1ebd8316ff0e67e52d38
Adam Volkman <itismaku@gmail.com>
2021-10-25 12:17:02 +0200

    project

---
+++ src/main/java/cz/maku/anticheststealer/AntiChestStealer.java
@@ -0,0 +1,17 @@
+package cz.maku.anticheststealer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiChestStealer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
